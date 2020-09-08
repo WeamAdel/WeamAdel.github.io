af2fe74b706dd9074d86ad8986d8f645183230d4
Weam Adel <weamadel1994@gmail.com>
2020-09-08 20:29:47 +0200

    Updates

---
+++ precache-manifest.64f062e7a1de8fab946bd991acff07b5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f861df91f9a3529d76faa3f3d3db7ce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3739d03ee9be4ad59ff6",
+    "url": "/static/css/2.22921e0d.chunk.css"
+  },
+  {
+    "revision": "3739d03ee9be4ad59ff6",
+    "url": "/static/js/2.323fdd5c.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/static/js/2.323fdd5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbbf07597eb26922fd47",
+    "url": "/static/js/main.11d77e94.chunk.js"
+  },
+  {
+    "revision": "45769461adb047b4b39b",
+    "url": "/static/js/runtime-main.03b47ed9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
